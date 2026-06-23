e776a9b952eb25e07edb95ef0da8e86bcef13dc3
gospelchima <gospelnkem@gmail.com>
2026-06-23 15:27:03 +0100

    about.tsx update

--- src/components/aboutus.tsx
+++ src/components/aboutus.tsx
@@ -26,16 +26,7 @@ const ClockIcon = () => (
   </svg>
 );
 
-const NairaIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <line x1="5" y1="5" x2="5" y2="19"/>
-    <line x1="19" y1="5" x2="19" y2="19"/>
-    <line x1="5" y1="9" x2="19" y2="15"/>
-    <line x1="5" y1="15" x2="19" y2="9"/>
-    <line x1="3" y1="9" x2="21" y2="9"/>
-    <line x1="3" y1="15" x2="21" y2="15"/>
-  </svg>
-);
+
 
 const whyItems: WhyItem[] = [
   {
